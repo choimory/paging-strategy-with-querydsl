3f7853264e2e36f58464cd27803ffb237b3cd6d3
choimory <choimory@naver.com>
2021-06-12 00:35:03 +0900

    Initial commit

---
+++ src/main/java/com/choimory/pagingstrategywithquerydsl/PagingStrategyWithQuerydslApplication.java
@@ -0,0 +1,13 @@
+package com.choimory.pagingstrategywithquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagingStrategyWithQuerydslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PagingStrategyWithQuerydslApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/choimory/pagingstrategywithquerydsl/PagingStrategyWithQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choimory.pagingstrategywithquerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagingStrategyWithQuerydslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
