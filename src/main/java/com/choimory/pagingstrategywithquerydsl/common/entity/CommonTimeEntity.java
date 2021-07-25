776f985f6fc2b58bbdff843d68505866c48438d3
choimory <43328589+choimory@users.noreply.github.com>
2021-07-25 18:05:00 +0900

    Audit 엔티티 클래스 빌더 추가

--- src/main/java/com/choimory/pagingstrategywithquerydsl/common/entity/CommonTimeEntity.java
+++ src/main/java/com/choimory/pagingstrategywithquerydsl/common/entity/CommonTimeEntity.java
@@ -1,5 +1,8 @@
 package com.choimory.pagingstrategywithquerydsl.common.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,6 +13,9 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommonTimeEntity {
     @CreatedDate
     private LocalDateTime createdTime;
